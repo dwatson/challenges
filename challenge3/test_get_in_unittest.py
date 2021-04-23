a6c9821fa94d2501519ae15a86add603dbcb19dc
David Watson <david@bashton.com>
2021-04-23 17:00:06 +0100

    Add test for branched source data.

--- challenge3/test_get_in_unittest.py
+++ challenge3/test_get_in_unittest.py
@@ -5,6 +5,7 @@ from GetIn import GetIn
 obj1 = {"a":{"b":{"c":"d"}}}
 obj2 = {"x": {"y": {"z": "a"}}}
 obj3 = {"a":{"b":{"c":{"d":{"e":{"f":"g"}}}}}}
+obj4 = {"a":{"b":{"c":"d"},"e":{"f":"g"}}}
 
 class TestGetIn(unittest.TestCase):
     def test_input_1(self):
@@ -23,5 +24,10 @@ class TestGetIn(unittest.TestCase):
     def test_missing_key_end(self):
         self.assertEqual(GetIn(obj1, "a/b/x"), None, "Should be None")
 
+    def test_branched_1(self):
+        self.assertEqual(GetIn(obj4, "a/b/c"), "d", "Should be d")
+
+    def test_branched_2(self):
+        self.assertEqual(GetIn(obj4, "a/e/f"), "g", "Should be g")
 if __name__ == '__main__':
     unittest.main()
